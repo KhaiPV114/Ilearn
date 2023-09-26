7541b6a008f0b240c3a63c431e7fe4aaab3c5317
Nguyen Son <sonnvqhe176602@fpt.edu.vn>
2023-09-26 20:21:23 +0700

    OL-5 Fix models, add GoogleUser
    - Change ERole to Role, remove ROLE_ prefix
    - Add GoogleUser POJO

---
+++ src/main/java/com/onlinelearning/Models/GoogleUser.java
@@ -0,0 +1,17 @@
+package com.onlinelearning.Models;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class GoogleUser {
+
+    private String email;
+    private boolean emailVerifired;
+    private String name;
+    private String pictureUrl;
+    private String locale;
+    private String familyName;
+    private String givenName;
+}
