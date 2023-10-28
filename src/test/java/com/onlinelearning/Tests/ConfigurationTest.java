9c987799e8145191fd74467fae75f8991978dba1
DuyDucLH <DuyLHDHE172114@fpt.edu.vn>
2023-10-28 16:35:20 +0700

    DuyLHD/Adding test private key of AES

--- src/test/java/com/onlinelearning/Tests/ConfigurationTest.java
+++ src/test/java/com/onlinelearning/Tests/ConfigurationTest.java
@@ -3,7 +3,8 @@ package com.onlinelearning.Tests;
 import com.onlinelearning.DAL.DBContext;
 import com.onlinelearning.DAL.Impl.DBContextImpl;
 import com.onlinelearning.Utils.DotEnv;
-import io.github.cdimascio.dotenv.Dotenv;
+import com.onlinelearning.Utils.Encryption;
+import com.onlinelearning.Utils.Impl.AESEncryptionImpl;
 import java.sql.Connection;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Order;
@@ -24,5 +25,14 @@ public class ConfigurationTest {
         DBContext dbContext = new DBContextImpl();
         Connection connection = dbContext.getConnection();
         Assertions.assertNotNull(connection);
+    }
+    
+    @Order(3)
+    @Test
+    public void testAESPrivateKey() {
+        String originalString = "Chac ai do se ve";
+        Encryption aes = new AESEncryptionImpl();
+        String encrypted = "rL9W+qfxxiBepJRsMlpmhLa7qneRqNIcfThjnKFMQ2I=";
+        Assertions.assertEquals(originalString, aes.decrypt(encrypted));
     }
 }
